138a8bf9b7bd2f6dc902ce8025f4d619dfb00e7c
pradeeshan <pradeeshannagarajan@gmail.com>
2025-07-23 12:09:29 +0530

    db

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
+// PostgreSQL client
+const pool = new Pool({
+  user: 'user',
+  host: 'postgres', // Kubernetes service name
+  database: 'testdb',
+  password: 'password',
+  port: 5432,
+});
+
 app.get('/', (req, res) => {
   res.send('Hello from Node.js running in KinD Kubernetes Cluster!');
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`DB Time: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Database connection failed');
+  }
 });
 
 app.listen(port, '0.0.0.0', () => {
